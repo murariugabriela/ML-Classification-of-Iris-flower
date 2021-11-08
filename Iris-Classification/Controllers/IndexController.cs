3a6898d54955faa8f1cf6c081d54bfbf771553cc
murariugabriela <murariugabriela01@gmail.com>
2021-11-08 16:11:40 +0200

    Added first project structure

---
+++ Iris-Classification/Controllers/IndexController.cs
@@ -0,0 +1,23 @@
+﻿/// <summary>
+/// Summary description for Class1
+/// </summary>
+namespace Iris_Classification.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class IndexController : ControllerBase
+	{
+        private readonly ILogger<Index> _logger;
+
+        public IndexController(ILogger<Index> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "hello";
+        }
+    }
+}
